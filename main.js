063d55c377655fbe3c257637afdc388eeb007fe1
Warckor <mik.el.loco@gmail.com>
2023-09-06 23:56:12 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+const operators = ['+', '-', '*', '/'];
+let firstNumber = 0;
+let secondNumber = 0;
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const operate = (a, b, operator) => {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
+}
