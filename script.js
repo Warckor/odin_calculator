ffc79a8c9c06606a31b28807baf3565ef7b8105c
u029299 <miguel.gorostola.ext@rsi.cajarural.com>
2023-09-07 18:47:07 +0200

    Add operations with two values

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const display = document.getElementById('display')
 const buttons = document.querySelectorAll('button')
 let firstNumber = 0
 let secondNumber = 0
+let operator = ''
 
 const add = (a, b) => a + b
 const subtract = (a, b) => a - b
@@ -16,7 +17,7 @@ const operate = (a, b, operator) => {
       return subtract(a, b)
     case '*':
       return multiply(a, b)
-    case '/':
+    case '÷':
       return divide(a, b)
   }
 }
@@ -25,35 +26,42 @@ buttons.forEach(button => {
   button.addEventListener('click', e => {
     const buttonId = e.target.id
     const value = e.target.textContent
+    if (display.value === 'Error') {
+      display.value = ''
+    }
     if (buttonId === 'clear') {
       display.value = ''
       firstNumber = 0
       secondNumber = 0
+      operator = ''
     } else if (buttonId === 'backspace') {
       display.value = display.value.slice(0, -1)
     } else if (buttonId === 'divide') {
-      if (firstNumber !== 0) {
-        secondNumber = display.value
-      }
+      operator = value
       firstNumber = display.value
+      display.value = ''
     } else if (buttonId === 'multiply') {
-      if (firstNumber !== 0) {
-        secondNumber = display.value
-      }
+      operator = value
       firstNumber = display.value
+      display.value = ''
     } else if (buttonId === 'subtract') {
-      if (firstNumber !== 0) {
-        secondNumber = display.value
-      }
+      operator = value
       firstNumber = display.value
+      display.value = ''
     } else if (buttonId === 'add') {
-      if (firstNumber !== 0) {
-        secondNumber = display.value
-      }
+      operator = value
       firstNumber = display.value
+      display.value = ''
     } else if (buttonId === 'equals') {
-      firstNumber = 0
-      secondNumber = 0
+      if (operator === '') {
+        display.value = 'Error'
+      } else {
+        secondNumber = display.value
+        display.value = operate(Number(firstNumber), Number(secondNumber), operator)
+        firstNumber = 0
+        secondNumber = 0
+        operator = ''
+      }
     } else {
       display.value += value
     }
